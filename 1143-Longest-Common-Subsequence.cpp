0553dd820b40cec191b8a90c00ffc70e4c041440
Haardik Madaan <madaanhaardik@gmail.com>
2026-07-28 18:26:32 +0530

    [1143] [Time Beats: 65.00%] [Memory Beats: 6.19%] - LeetPush

--- 1143-Longest-Common-Subsequence.cpp
+++ 1143-Longest-Common-Subsequence.cpp
@@ -21,20 +21,21 @@ public:
     }
 
     int solve2(vector<vector<int>> &dp, string &text1, string& text2, int n1, int n2){
-
+        
+        vector<int> prev(n2+1,0);
         
         for (int i = n1-1;i>=0;i--){
+            vector<int> curr(n2+1,0);
             for (int j = n2-1;j>=0;j--){
-
                 if(text1[i] == text2[j]){
-                    dp[i][j] = 1 + dp[i+1][j+1];
+                    curr[j] = 1 + prev[j+1];
                 }
                 else{
-                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                    curr[j] = max(prev[j],curr[j+1]);
                 } 
             }
+            prev = curr;
         }
-
-        return dp[0][0];
+        return prev[0];
     }
 };
