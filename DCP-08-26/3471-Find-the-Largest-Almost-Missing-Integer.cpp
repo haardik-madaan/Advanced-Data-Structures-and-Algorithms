07b264224994e2170b54ae2807e043d399e7464e
Haardik Madaan <madaanhaardik@gmail.com>
2026-08-18 13:57:18 +0530

    [3471] [Time Beats: 100.00%] [Memory Beats: 40.98%] - LeetPush

--- DCP-08-26/3471-Find-the-Largest-Almost-Missing-Integer.cpp
+++ DCP-08-26/3471-Find-the-Largest-Almost-Missing-Integer.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        unordered_map<int, unordered_set<int>> mpp;
+        int maxEl = *max_element(nums.begin(),nums.end());
+        vector<int> ranks(maxEl+1,0);
+        vector<int> canBeAns(maxEl+1,0);
+        for (int i = 0;i<nums.size();i++){
+            canBeAns[nums[i]] = 1;
+        }
         int n = nums.size();
         int rank = 1;
         for (int i = 0;i<n;i++){
             for(int j = 0;j<k;j++){
                 if(i+k-1<n){
-                    mpp[nums[i+j]].insert(rank);
+                    if(ranks[nums[i+j]] == rank){
+                        continue;
+                    }
+                    else if(ranks[nums[i+j]] == 0){
+                        ranks[nums[i+j]] = rank;
+                    }
+                    else{
+                        canBeAns[nums[i+j]] = 0;
+                        ranks[nums[i+j]] == rank;
+
+                    }
                 }
             }
             rank++;
         }
-        int element = -1;
-        for (auto it : mpp){
-            int el = it.first;
-            if(mpp[el].size() == 1){
-                element = max(element,el);
+        for (int i = maxEl;i>=0;i--){
+            if(canBeAns[i] == 1){
+                return i;
             }
         }
-        return element;
+        return -1;
     }
 };
