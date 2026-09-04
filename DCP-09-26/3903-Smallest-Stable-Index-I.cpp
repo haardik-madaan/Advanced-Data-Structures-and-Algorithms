3fecb5a8dc4180ff61d5104d38943cc5b9f06064
Haardik Madaan <madaanhaardik@gmail.com>
2026-09-04 14:38:22 +0530

    [3903] [Time Beats: 100.00%] [Memory Beats: 46.94%] - LeetPush

---
+++ DCP-09-26/3903-Smallest-Stable-Index-I.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        vector<int> maxes;
+        vector<int> minis;
+
+        int n = nums.size();
+        int maxi = 0;
+        int mini = INT_MAX;
+        for (int i = 0;i<n;i++){
+            maxi = max(maxi,nums[i]);
+            maxes.push_back(maxi);
+            mini = min(mini,nums[n-i-1]);
+            minis.push_back(mini);
+        }
+
+        reverse(minis.begin(),minis.end());
+
+        for (int i = 0;i<n;i++){
+            if(maxes[i] - minis[i] <= k){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
